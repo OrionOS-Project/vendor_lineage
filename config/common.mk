86853dee5ff15601d933d1708da1459cd4e95468
El Dainosor <eldainosor@gmail.com>
2022-07-31 05:38:19 -0300

    config: Disable DocumentsUI icon
    Change-Id: I658958aca6014d62bc68e231f92e857d5b899af5

--- config/common.mk
+++ config/common.mk
@@ -77,6 +77,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-system_ext.rc
 
+# our sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/orion-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/orion-sysconfig.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
