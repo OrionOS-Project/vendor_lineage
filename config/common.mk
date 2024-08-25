81cebc811f527bc2124904927e9dc4ffa9b935e3
LuK1337 <priv.luk@gmail.com>
2024-08-25 22:12:21 +0200

    config: Set ro.opa.eligible_device=true
    This enables hotword detection for Google Assistant.
    
    [Linux4: Make it overridable for devices where this breaks mic]
    
    Change-Id: If1f7b77ca3ea907fceb945d7cdaf1bea253c00c9

--- config/common.mk
+++ config/common.mk
@@ -201,6 +201,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Google Assistant
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device?=true
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
