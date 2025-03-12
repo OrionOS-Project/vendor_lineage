afd837b7ff6f2178a6a483bc2d4beef357308b9a
slogar25 <michaelslogar@gmail.com>
2025-03-12 22:31:04 +0000

    overlay: build trafficlight overlay
    Change-Id: Id7f5063186a2f891859ab852553d4970e159e888
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- config/orion.mk
+++ config/orion.mk
@@ -70,14 +70,19 @@ PRODUCT_PACKAGES += \
 	Miniature \
 	SoundPickerPrebuilt_32000142
 
-# FaceUnlock
-ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
-PRODUCT_PACKAGES += \
-    FaceUnlock
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.face.sense_service=true
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+    PRODUCT_PACKAGES += \
+        ParanoidSense
+    PRODUCT_SYSTEM_EXT_PROPERTIES += \
+        ro.face.sense_service=true
+    PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+else
+    PRODUCT_PACKAGES += \
+        SettingsGoogleFutureFaceEnroll \
+        PixelTrafficLightFaceOverlay
 endif
 
 # GApps
