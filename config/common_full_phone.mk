d38ad450a01ada8ffa22657b9b64fd505edc837d
Ido Ben-Hur <idoybh2@gmail.com>
2025-07-14 11:38:52 +0300

    config: Enable Bluetooth auto turn on by default
    [nurkeinneid: make it overridable]
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -5,8 +5,9 @@ $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 PRODUCT_PACKAGES += \
     UdfpsAnimations
 
-# Enable support of one-handed mode
+# Enable support of one-handed mode and BT auto-on
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode?=true
+    ro.support_one_handed_mode?=true \
+    bluetooth.server.automatic_turn_on?=true
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
