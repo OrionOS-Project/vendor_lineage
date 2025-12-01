DerpFest-AOSP/vendor_derp

derp: Create copy of angle privapp permission in vendor
* AOSP fixed in android-14.0.0_r17:
ref:https://android.googlesource.com/platform/external/angle/+/3697a05a0617ec5487b242ee8a972410a53d3743%5E2..3697a05a0617ec5487b242ee8a972410a53d3743/

error:
12-19 18:11:07.523  3166  3166 E AndroidRuntime: *** FATAL EXCEPTION IN SYSTEM PROCESS: main
12-19 18:11:07.523  3166  3166 E AndroidRuntime: java.lang.IllegalStateException: Signature|privileged permissions not in privapp-permissions allowlist: {com.android.angle (/product/priv-app/ANGLE): android.permission.WRITE_SECURE_SETTINGS}
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.pm.permission.PermissionManagerServiceImpl.onSystemReady(PermissionManagerServiceImpl.java:4447)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.pm.permission.PermissionManagerService$PermissionManagerServiceInternalImpl.onSystemReady(PermissionManagerService.java:743)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.pm.PackageManagerService.systemReady(PackageManagerService.java:4332)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.SystemServer.startOtherServices(SystemServer.java:2831)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.SystemServer.run(SystemServer.java:956)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.server.SystemServer.main(SystemServer.java:678)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at java.lang.reflect.Method.invoke(Native Method)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
12-19 18:11:07.523  3166  3166 E AndroidRuntime:   at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:856)

Change-Id: I38e6de3d8f1223b9ba77ecb58650b9e1bdf35e48
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

config/common.mk
@@ -106,6 +106,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/etc/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
 
+# ANGLE
+PRODUCT_COPY_FILES += \
+    vendor/derp/prebuilt/common/etc/permissions/product-privapp-permissions-aosp.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-privapp-permissions-aosp.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.

OrionOS-Project/vendor_lineage

config/common.mk
@@ -109,6 +109,10 @@ endif
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# ANGLE
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/permissions/product-privapp-permissions-aosp.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-privapp-permissions-aosp.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.